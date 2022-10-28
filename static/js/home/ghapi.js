9333a1e68e2843d92acf98c4ade4795cc1ee30ee
Nicholas Sidharta <toxicancm@gmail.com>
2022-10-28 15:34:45 +0700

    GH API Finish

--- static/js/home/ghapi.js
+++ static/js/home/ghapi.js
@@ -1,12 +1,22 @@
 // Octokit.js
 // https://github.com/octokit/core.js#readme
+
+import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
+
 const octokit = new Octokit({
     auth: '${{ secrets.GHAPI }}'
   })
   
-  await octokit.request('GET /repos/Nicholas-Sidharta12365/tugas-kelompok-pbp/collaborators', {
+const response = await octokit.request('GET /repos/Nicholas-Sidharta12365/tugas-kelompok-pbp/collaborators', {
     owner: 'OWNER',
     repo: 'REPO'
   })
 
-  
+let tags = "";
+
+for(let i = 0 ; i < response.data.length; i++){
+  console.log(response.data[i].avatar_url);
+  tags += "<img src=" + response.data[i].avatar_url + "alt=pfp class='rounded-full h-12 w-12 mx-1'></img>"
+}
+
+document.getElementById("collab_list").innerHTML = tags;
